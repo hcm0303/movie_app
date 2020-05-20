42c34244945c5293443dbf6b36dbab4560967f95
Hyunchul Ma <hcm0303@gmail.com>
2020-05-20 11:26:21 +0900

    Updates

---
+++ precache-manifest.4f3ca082f7e37d8156c9fb949fc8363e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4df8a569a18926ff88f9ba81a0c18f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "257e6813ed4d1cbc2a22",
+    "url": "/movie_app/static/css/main.23808f81.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257e6813ed4d1cbc2a22",
+    "url": "/movie_app/static/js/main.b812dff0.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
